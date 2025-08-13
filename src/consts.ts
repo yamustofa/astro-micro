bc7e179536ebccf04b752254b88f0a8e5f23bdb7
xhabibmustofax <xhabibmustofax@gmail.com>
2025-08-13 17:17:40 +0700

    feat: customize

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Little Code Journey",
+  DESCRIPTION: "Blog to documented my little code journey.",
+  EMAIL: "habibmustofa.dev@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "Little Code Journey.",
 };
 
 export const BLOG: Metadata = {
@@ -26,15 +26,15 @@ export const PROJECTS: Metadata = {
 
 export const SOCIALS: Socials = [
   {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheek",
+    NAME: "X",
+    HREF: "https://x.com/",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/yamustofa",
   },
   {
     NAME: "Website",
-    HREF: "https://trevortylerlee.com",
+    HREF: "https://astro-micro-umber.vercel.app/",
   },
 ];
